5f5d222fadc83ea2179cfd155b3709db1b0e3668
snehagoyal25 <goyalsneha995@gmail.com>
2025-11-11 10:54:53 +0530

    backend

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,13 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 // const cookieParser = require('cookie-parser');
-// const connectToDb = require('./db/db');
+const connectToDb = require('./db/db');
 // const userRoutes = require('./routes/user.routes');
 // const captainRoutes = require('./routes/captain.routes');
 // const mapsRoutes = require('./routes/maps.routes');
 // const rideRoutes = require('./routes/ride.routes');
 
-// connectToDb();
+connectToDb();
 
 app.use(cors());
 // app.use(express.json());
